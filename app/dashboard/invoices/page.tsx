19ac6ce4798faa6464c5cf6113cf64826ca28765
Jojo Thomas <jojothomas1515@gmail.com>
2025-07-06 06:17:29 +0100

    initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Dashboard | Invoices"
+}
+
+export default function Invoices() {
+  return <p>Invoices Page</p>;
+}
